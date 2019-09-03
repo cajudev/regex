f0624383c5563b7aed5fa0f9e142b6e38bcf376c
lopesrichard <richard.lopes@branvo.com>
2019-09-03 13:32:24 -0300

    error handler improvement

--- src/Regex.php
+++ src/Regex.php
@@ -10,6 +10,13 @@ class Regex {
         $this->parse($pattern);
     }
 
+    /**
+     * Perform a regular expression match
+     *
+     * @param  string $subject
+     *
+     * @return bool
+     */
     public function match(string $subject): bool {
         if ($this->parser->isGlobal()) {
             return preg_match_all($this->parser->getExpression(), $subject, $this->matches);
@@ -17,18 +24,51 @@ class Regex {
         return preg_match($this->parser->getExpression(), $subject, $this->matches);
     }
 
+    /**
+     * Split a string by regular expresion
+     *
+     * @param  string $subject
+     * @param  int    $limit
+     * @param  int    $flags
+     *
+     * @return array
+     */
     public function split(string $subject, int $limit = -1, int $flags = 0): array {
         return preg_split($this->parser->getExpression(), $subject, $limit, $flags);
     }
 
+    /**
+     * Perform a regular expression search and replace
+     *
+     * @param  string $subject
+     * @param  string $replace
+     * @param  int    $limit
+     * @param  int    $count
+     *
+     * @return string
+     */
     public function replace(string $subject, string $replace, int $limit = -1, int &$count = 0): string {
         return preg_replace($this->parser->getExpression(), $replace, $subject, $limit, $count);
     }
     
+    /**
+     * Get the result of a previous match
+     *
+     * @param  string $key
+     *
+     * @return mixed
+     */
     public function get(string $key = '0') {
         return $this->matches[$key] ?? false;
     }
 
+    /**
+     * Parse the current regular expression
+     *
+     * @param  string $pattern
+     *
+     * @return void
+     */
     private function parse(string $pattern) {
         $this->parser = new RegexParser($pattern);
         $this->parser->parse();
--- src/RegexParser.php
+++ src/RegexParser.php
@@ -5,6 +5,7 @@ namespace Cajudev;
 class RegexParser {
     private $pattern;
     private $global;
+    private $errorMessage;
 
     public function __construct(string $pattern) {
         $this->pattern = $pattern;
@@ -12,24 +13,103 @@ class RegexParser {
     }
 
     public function parse() {
-        set_error_handler([$this, 'handler'], E_WARNING);
-        preg_match($this->pattern, 'error test');
+        $this->setHandler();
+        $this->makeSimpleTest();
+        $this->restoreHandler();
+        $this->throwExceptionIfError();
+    }
+
+    /**
+     * Setup the temporary custom warning error handler
+     *
+     * @return void
+     */
+    private function setHandler() {
+        set_error_handler([$this, 'handleError'], E_WARNING);
+    }
+
+    /**
+     * Do a simple preg_match built-in function just to test if any error will be thrown
+     *
+     * @return void
+     */
+    private function makeSimpleTest() {
+        preg_match($this->pattern, 'string test');
+    }
+
+    /**
+     * Parse the error message, setting up as global when modifier 'g' was used
+     * otherwise, set the corresponding error message
+     *
+     * @param  int    $errno
+     * @param  string $errstr
+     *
+     * @return void
+     */
+    public function handleError(int $errno , string $errstr) {
+        $errorMessage = substr($errstr, 14);
+        if ($errorMessage !== "Unknown modifier 'g'") {  
+            $this->setErrorMessage($errorMessage);
+        } else {
+            $this->setAsGlobal();
+        }
+    }
+
+    /**
+     * Restore the original error handler
+     *
+     * @return void
+     */
+    private function restoreHandler() {
         restore_error_handler();
     }
 
-    private function handler(int $errno , string $errstr) {
-        $errorMessage = substr($errstr, 14);
-        if ($errorMessage !== "Unknown modifier 'g'") {
-            throw new RegularExpressionException($errorMessage);
+    /**
+     * If any error message was set, throws a exception
+     *
+     * @return void
+     */
+    private function throwExceptionIfError() {
+        if ($this->errorMessage) {
+            throw new RegularExpressionException($this->errorMessage);
         }
+    }
+
+    /**
+     * Set the error message
+     *
+     * @param  string $errorMessage
+     *
+     * @return void
+     */
+    private function setErrorMessage(string $errorMessage) {
+        $this->errorMessage = $errorMessage;
+    }
+
+    /**
+     * Set the current regular expression as global
+     *
+     * @return void
+     */
+    private function setAsGlobal() {
         $this->global  = true;
         $this->pattern = preg_replace('/(.+)(g)/', '\\1', $this->pattern);
     }
 
+    /**
+     * Return the current regular expression
+     *
+     * @return string
+     */
     public function getExpression(): string {
         return $this->pattern;
     }
 
+    /**
+     * Inform whether the current regular expression is global or not
+     *
+     * @return bool
+     */
     public function isGlobal(): bool {
         return $this->global;
     }
